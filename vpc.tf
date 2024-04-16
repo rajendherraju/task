f7418fa5d3a99f435ae66a5cac95343790837b11
rajendherraju <38126617+rajendherraju@users.noreply.github.com>
2024-04-17 00:31:18 +0530

    create subnets

--- vpc.tf
+++ vpc.tf
@@ -14,5 +14,53 @@ resource "aws_vpc" "main" {
 }
 
 # =========================
-# Create your subnets here
+# sub nets
 # =========================
+module "subnets" {
+  source  = "hashicorp/subnets/cidr"
+  version = "1.0.0"
+
+  vpc_cidr_block = var.vpc_cidr
+  azs            = data.aws_availability_zones.available.names
+
+  # Define the number of subnets needed
+  number_of_subnet_bits = 1  # Using 1 bit for subnetting (2^1 = 2 subnets)
+  newbits               = 1  # Using 1 bit for subnetting (2^1 = 2 subnets)
+
+  tags = module.label_vpc.tags
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = module.subnets.subnets[0].cidr_block
+  availability_zone = module.subnets.subnets[0].availability_zone
+  tags              = module.label_vpc.tags
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = module.subnets.subnets[1].cidr_block
+  availability_zone = module.subnets.subnets[1].availability_zone
+  tags              = module.label_vpc.tags
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = module.label_vpc.tags
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags   = module.label_vpc.tags
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
