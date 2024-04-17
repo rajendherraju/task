d407b8546dc387205c1aa75712eb7e781474263f
rajendherraju <38126617+rajendherraju@users.noreply.github.com>
2024-04-17 16:51:22 +0530

    new code

--- vpc.tf
+++ vpc.tf
@@ -16,58 +16,19 @@ resource "aws_vpc" "main" {
 # =========================
 # sub nets
 # =========================
-module "label_vpc" {
-  source     = "cloudposse/label/null"
-  version    = "0.25.0"
-  context    = module.base_label.context
-  name       = "vpc"
-  attributes = ["main"]
-}
-
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags                 = module.label_vpc.tags
-}
 
 data "aws_availability_zones" "available" {}
 
-# Create public subnet
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 4, 0)  # Example subnet CIDR block
-  availability_zone = data.aws_availability_zones.available.names[0]  # Example availability zone
-  tags              = module.label_vpc.tags
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, 0) // Example subnet CIDR block
+  availability_zone = US-EAST-1 
+  tags              = merge(module.label_vpc.tags, { Name = "public-subnet" })
 }
 
-# Create private subnet
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 4, 1)  # Example subnet CIDR block
-  availability_zone = data.aws_availability_zones.available.names[1]  # Example availability zone
-  tags              = module.label_vpc.tags
-}
-
-# Create internet gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags   = module.label_vpc.tags
-}
-
-# Create public route table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  tags   = module.label_vpc.tags
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-# Associate public subnet with public route table
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, 1) // Example subnet CIDR block
+  availability_zone =US-EAST-1 
+  tags              = merge(module.label_vpc.tags, { Name = "private-subnet" })
 }
